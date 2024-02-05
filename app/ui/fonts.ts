ffc16f922630e8900b40b03e86a2ca901a54eaa8
Vico <vico@vico.com>
2024-02-05 21:57:36 +0100

    start

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Montserrat_Alternates } from 'next/font/google';
+
+export const montserrat = Montserrat_Alternates({
+    weight: ['200', '300', '400', '500'],
+    subsets: ['latin']
+});
+
+export const lusitana = Lusitana({
+    weight: ['400'],
+    subsets: ['latin']
+});
